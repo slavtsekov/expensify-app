59aed1fe2a3998a0f6a6d3461fd504a1d2b97e4f
Slav Tsekov <slav.tsekov@gmail.com>
2018-02-03 12:52:44 +0200

    Add basic routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>My React boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>This is my add expense component</div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is my help component
+    </div>
+);
+
+const router = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />  
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />      
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(router, document.getElementById("app"));
