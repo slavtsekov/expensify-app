86a2587ff6913d38e32485f562445ab66406092d
Slav Tsekov <slav.tsekov@gmail.com>
2018-03-12 20:03:59 +0200

    Add test cases for ExpenseListFilters

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import moment from "moment";
 import { ExpenseListFilters } from "../../components/ExpenseListFilters";
 import { defaultFilters, filters } from "../fixtures/filters";
 
@@ -36,6 +37,60 @@ test("should render ExpenseListFilters with custom filters", () => {
     wrapper.setProps({
         filters
     });
-    
+
     expect(wrapper).toMatchSnapshot();
+});
+
+test("should handle text change", () => {
+    const text = "rent";
+
+    wrapper.find("input").simulate("change", {
+        target: {
+            value: text
+        }
+    });
+
+    expect(setTextFilterMock).toHaveBeenLastCalledWith(text);
+});
+
+test("should sort by amount", () => {
+    wrapper.find("select").simulate("change", {
+        target: {
+            value: "amount"
+        }
+    });
+
+    expect(sortByAmountMock).toHaveBeenCalled();
+});
+
+test("should sort by date", () => {
+    wrapper.setProps({
+        filters
+    });
+
+    wrapper.find("select").simulate("change", {
+        target: {
+            value: "date"
+        }
+    });
+
+    expect(sortByDateMock).toHaveBeenCalled();
+});
+
+test("should handle date changes", () => {
+    const startDate = moment().add(1, "days");
+    const endDate = moment().add(3, "days");
+
+    wrapper.find("DateRangePicker").prop("onDatesChange")({ startDate, endDate });
+
+    expect(setStartDateMock).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDateMock).toHaveBeenLastCalledWith(endDate);
+});
+
+test("should handle date focus changes", () => {
+    const input = "startDate";
+
+    wrapper.find("DateRangePicker").prop("onFocusChange")(input);
+
+    expect(wrapper.state("focusedDateInput")).toBe(input);
 });
