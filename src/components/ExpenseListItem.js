1ae5cb8c6cac63bb5e009e6b3e268196c8c03142
Slav Tsekov <slav.tsekov@gmail.com>
2018-04-05 14:16:09 +0300

    Add formatting to expense list items

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 const ExpenseListItem = (props) => {
     const { id, description, amount, createdAt } = props.data;
     return (
         <div>
-            <Link to={`/edit/${id}`}>{description}</Link> - {amount} - {createdAt}
+            <Link to={`/edit/${id}`}>{description}</Link>
+            - 
+            {numeral(amount/100).format("$0,0.00")} 
+            - 
+            {moment(createdAt).format("MMMM Do, YYYY")}
         </div>
     );
 };
