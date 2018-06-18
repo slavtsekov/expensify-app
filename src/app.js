bcbbfa5871b5ada7c9235e5673146fd4bfbca2b5
Slav Tsekov <slav.tsekov@gmail.com>
2018-06-18 14:05:54 +0300

    Move authentication code in a module

--- src/app.js
+++ src/app.js
@@ -1,49 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import AppRouter, { history } from "./routers/AppRouter";
+import AppRouter from "./routers/AppRouter";
 import getStore from "./store/configureStore";
-import { requestSetExpenses } from "./actions/expenses";
-import { login, logout } from "./actions/auth";
-import getVisibleExpenses from "./selectors/expenses";
-import { firebase } from "./firebase/firebase";
+import "./firebase/firebase";
 import LoadingPage from "./components/LoadingPage";
+import configureAuthentication from "./firebase/configureAuthentication";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = getStore();
-
-const template = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-);
-
 let hasRendered = false;
 const renderApp = () => {
-    if (!hasRendered) {
-        ReactDOM.render(template, document.getElementById("app"));
-        hasRendered = true;
-    }
+    if (hasRendered) return;
+    const template = (
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
+    );
+    ReactDOM.render(template, document.getElementById("app"));
+    hasRendered = true;
 };
-
 ReactDOM.render(<LoadingPage />, document.getElementById("app"));
-
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        store.dispatch(login(user.uid));
-        store.dispatch(requestSetExpenses(() => {
-            renderApp();
-            if (history.location.pathname === "/") {
-                history.push("/dashboard");
-            }
-        }));
-    } else {
-        store.dispatch(logout());
-        renderApp();
-        history.push("/");
-    }
-});
+configureAuthentication(renderApp, store);
 
